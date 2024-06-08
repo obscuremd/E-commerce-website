f960ce42b34e11e28e3b6e716d4f69ca2960b434
dan <md.erhenede@gmail.com>
2024-06-08 04:46:44 +0100

    modified cart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,25 +1,196 @@
 import React, { useContext } from 'react'
-import { useRecoilState} from 'recoil'
+import { useRecoilState } from 'recoil'
 import { CartState } from '../atoms/CartState'
-import { Avatar, Card, List, ListItem, ListItemPrefix, Typography } from '@material-tailwind/react'
-import { TrashIcon } from '@heroicons/react/16/solid'
+import { Avatar, Button, Card, CardBody, CardFooter, CardHeader, Chip, IconButton, Input, List, ListItem, ListItemPrefix, Tab, Tabs, TabsHeader, Tooltip, Typography } from '@material-tailwind/react'
+import { ChevronUpDownIcon, MagnifyingGlassIcon, PencilIcon, TrashIcon, UserPlusIcon } from '@heroicons/react/16/solid'
+import { Link } from 'react-router-dom'
 
 
+const TABS = [
+  {
+    label: "All",
+    value: "all",
+  },
+  {
+    label: "Local",
+    value: "Local",
+  },
+  {
+    label: "International",
+    value: "International",
+  },
+];
 
+const TABLE_HEAD = ["Item", "Price", "Total", ""];
 
 const CartItem = () => {
-    const [cart , setCart] = useRecoilState(CartState)
+  const [cart, setCart] = useRecoilState(CartState)
 
-    console.log(cart)
+  console.log(cart)
 
-    const handleRemove = (id) => {
-      setCart((prevCart) => prevCart.filter(item => item.id !== id));
+  const handleRemove = (id) => {
+    setCart((prevCart) => prevCart.filter(item => item.id !== id));
   };
 
   return (
-    <div className='md:py-24 md:px-44'>
 
-      <List className='w-full' >
+    < div className='py-12 md:px-[8%]'>
+      <Card className="h-full w-full">
+        <CardHeader floated={false} shadow={false} className="rounded-none">
+          <div className="mb-8 flex items-center justify-between gap-8">
+            <div>
+              <Typography variant="h5" color="blue-gray">
+                Cart list
+              </Typography>
+              <Typography color="gray" className="mt-1 font-normal">
+                Time to check out your cart
+              </Typography>
+            </div>
+            <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+              <Button variant="outlined" size="sm">
+                view all
+              </Button>
+              <Link to={'/'}>
+                <Button className="flex items-center gap-3" size="sm">
+                  <UserPlusIcon strokeWidth={2} className="h-4 w-4" /> Continue Shopping
+                </Button>
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+            <Tabs value="all" className="w-full md:w-max">
+              <TabsHeader>
+                {TABS.map(({ label, value }) => (
+                  <Tab key={value} value={value}>
+                    &nbsp;&nbsp;{label}&nbsp;&nbsp;
+                  </Tab>
+                ))}
+              </TabsHeader>
+            </Tabs>
+            <div className="w-full md:w-72">
+              <Input
+                label="Search"
+                icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+              />
+            </div>
+          </div>
+        </CardHeader>
+        <CardBody className="overflow-scroll px-0">
+          <table className="mt-4 w-full min-w-max table-auto text-left">
+            <thead>
+              <tr>
+                {TABLE_HEAD.map((head, index) => (
+                  <th
+                    key={head}
+                    className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
+                  >
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                    >
+                      {head}{" "}
+                      {index !== TABLE_HEAD.length - 1 && (
+                        <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
+                      )}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map(
+                (item, index) => {
+                  const isLast = index === cart.length - 1;
+                  const classes = isLast
+                    ? "p-4"
+                    : "p-4 border-b border-blue-gray-50";
+
+                  return (
+                    <tr key={cart.name}>
+                      <td className={classes}>
+                        <div className="flex items-center gap-3">
+                          <Avatar src={item.image} size="sm" />
+                          <div className="flex flex-col">
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="font-normal w-[15em] truncate"
+                            >
+                              {item.name}
+                            </Typography>
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="font-normal opacity-70 w-[20em] truncate"
+                            >
+                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus odit nemo, dolore ullam, corrupti nobis conse
+                            </Typography>
+                          </div>
+                        </div>
+                      </td>
+                      <td className={classes}>
+                        <div className="flex flex-col">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {item.new_price}
+                          </Typography>
+
+                        </div>
+                      </td>
+                      <td className={classes}>
+                        <div className="w-max">
+                          <Chip
+                            variant="ghost"
+                            size="sm"
+                            value={item.new_price * item.quantity}
+                            color="green"
+                          />
+                        </div>
+                      </td>
+                      <td className={classes}>
+                        <Tooltip content="Delete">
+                          <IconButton onClick={() => handleRemove(item.id)} variant="text">
+                            <TrashIcon className="h-4 w-4" />
+                          </IconButton>
+                        </Tooltip>
+                      </td>
+                    </tr>
+                  );
+                },
+              )}
+            </tbody>
+          </table>
+        </CardBody>
+        {/* <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+          <Typography variant="small" color="blue-gray" className="font-normal">
+            Page 1 of 10
+          </Typography>
+          <div className="flex gap-2">
+            <Button variant="outlined" size="sm">
+              Previous
+            </Button>
+            <Button variant="outlined" size="sm">
+              Next
+            </Button>
+          </div>
+        </CardFooter> */}
+      </Card>
+    </div >
+
+  )
+}
+
+export default CartItem
+
+
+
+// <div className='md:py-24 md:px-44'>
+
+{/* <List className='w-full' >
               <ListItem className='justify-between md:py-10' >
                 <ListItemPrefix>
                   <Typography variant="h6" color="blue-gray" className='md:w-[10em] truncate'>
@@ -73,8 +244,4 @@ const CartItem = () => {
         ))}
     </Card>
       
-    </div>
-  )
-}
-
-export default CartItem
+    // </div> */}
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { CartState } from "../atoms/CartState";
 import {
@@ -8,43 +8,43 @@ import {
 	CardFooter,
 	Typography,
 	Button,
-  } from "@material-tailwind/react";
+} from "@material-tailwind/react";
 import { motion } from "framer-motion";
 
-const Item = ({image, names, new_price, old_price, id, description}) => {
+const Item = ({ image, names, new_price, old_price, id, description }) => {
 
-	const [cart, setCart]=useRecoilState(CartState)
+	const [cart, setCart] = useRecoilState(CartState)
 
 	const product = {
-		name:names,
-		image:image,
-		new_price:new_price,
-		old_price:old_price,
-		id:id
+		name: names,
+		image: image,
+		new_price: new_price,
+		old_price: old_price,
+		id: id
 	}
 
 	const addToCart = (product) => {
-        setCart((prevCart) => {
-            const existingProduct = prevCart.find(item => item.id === product.id);
-            if (existingProduct) {
-                return prevCart.map(item => 
-                    item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
-                );
-            }
-            return [...prevCart, { ...product, quantity: 1 }];
-        });
-    };
+		setCart((prevCart) => {
+			const existingProduct = prevCart.find(item => item.id === product.id);
+			if (existingProduct) {
+				return prevCart.map(item =>
+					item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+				);
+			}
+			return [...prevCart, { ...product, quantity: 1 }];
+		});
+	};
 
 	return (
-		<motion.div whileHover={{scale:1.05}}>
+		<motion.div whileHover={{ scale: 1.05 }}>
 			<Card className="md:w-[290px] w-[150px] p-2">
 				<Link to={`/product/${id}`}>
-					<img 
-						src={image} 
-						alt="" 
+					<img
+						src={image}
+						alt=""
 						className="rounded-lg"
-						// onClick={window.scrollTo(0,0)}
-						/>
+						onClick={window.scrollTo(0, 0)}
+					/>
 				</Link>
 				<p className=" truncate">{names}</p>
 				<div className="flex flex-col gap-5">
@@ -59,53 +59,53 @@ const Item = ({image, names, new_price, old_price, id, description}) => {
 					</div>
 
 					<Button
-					onClick={()=>addToCart(product)}
-					ripple={false}
-					fullWidth={true}
-					className="bg-[#ff4141] text-white shadow-none "
+						onClick={() => addToCart(product)}
+						ripple={false}
+						fullWidth={true}
+						className="bg-[#ff4141] text-white shadow-none "
 					>
-					Add to Cart
+						Add to Cart
 					</Button>
 				</div>
 			</Card>
 		</motion.div>
-		
-	// 	<Card className="md:w-96">
-	// 	<CardHeader shadow={false} floated={false} className="h-96">
-	// 		<img
-	// 		src={image}
-	// 		alt="card-image"
-	// 		className="h-full w-full object-cover"
-	// 		/>
-	// 	</CardHeader>
-	// 	<CardBody>
-	// 		<div className="mb-2 flex items-center justify-between">
-	// 		<Typography color="blue-gray" className="font-medium truncate w-[10em]">
-	// 			{names}
-	// 		</Typography>
-	// 		<Typography color="blue-gray" className="font-medium">
-	// 			${new_price}.00
-	// 		</Typography>
-	// 		</div>
-	// 		<Typography
-	// 		variant="small"
-	// 		color="gray"
-	// 		className="font-normal opacity-75"
-	// 		>
-	// 		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate fugiat aut iusto Voluptate fugiat aut
-	// 		</Typography>
-	// 	</CardBody>
-	// 	<CardFooter className="pt-0">
-	// 		<Button
-	// 		onClick={()=>addToCart(product)}
-	// 		ripple={false}
-	// 		fullWidth={true}
-	// 		className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
-	// 		>
-	// 		Add to Cart
-	// 		</Button>
-	// 	</CardFooter>
-    // </Card>
+
+		// 	<Card className="md:w-96">
+		// 	<CardHeader shadow={false} floated={false} className="h-96">
+		// 		<img
+		// 		src={image}
+		// 		alt="card-image"
+		// 		className="h-full w-full object-cover"
+		// 		/>
+		// 	</CardHeader>
+		// 	<CardBody>
+		// 		<div className="mb-2 flex items-center justify-between">
+		// 		<Typography color="blue-gray" className="font-medium truncate w-[10em]">
+		// 			{names}
+		// 		</Typography>
+		// 		<Typography color="blue-gray" className="font-medium">
+		// 			${new_price}.00
+		// 		</Typography>
+		// 		</div>
+		// 		<Typography
+		// 		variant="small"
+		// 		color="gray"
+		// 		className="font-normal opacity-75"
+		// 		>
+		// 		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate fugiat aut iusto Voluptate fugiat aut
+		// 		</Typography>
+		// 	</CardBody>
+		// 	<CardFooter className="pt-0">
+		// 		<Button
+		// 		onClick={()=>addToCart(product)}
+		// 		ripple={false}
+		// 		fullWidth={true}
+		// 		className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+		// 		>
+		// 		Add to Cart
+		// 		</Button>
+		// 	</CardFooter>
+		// </Card>
 	);
 };
 
